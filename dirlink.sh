3d61c080360506955b9d4aa9b1529eeaddb2123c
appotry <andycrusoe@gmail.com>
2021-04-04 01:41:51 +0800

    Update dirlink.sh

--- dirlink.sh
+++ dirlink.sh
@@ -2,6 +2,7 @@
 #auther: andycrusoe@gmail.com
 #记录日志: ./dirlink.sh > dirlink.log
 #使用说明: https://github.com/appotry/PTtool#readme
+#https://gitee.com/bloodwolf/PTtool
 
 #查找文件硬链接
 #ls -ialh file.txt
