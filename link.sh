7842144a76ae3a1c44bf85dbe1466449baf2c2a3
appotry <andycrusoe@gmail.com>
2021-04-04 02:43:31 +0800

    Create link.sh

---
+++ link.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#一次性硬链接多个目录的使用方法实例脚本。
+#自用请修改源路径和目的路径
+/share/Download/source/dirlink.sh /share/Download/source/anime /share/Download/dst/anime
+/share/Download/source/dirlink.sh /share/Download/source/movie /share/Download/dst/movie
+/share/Download/source/dirlink.sh /share/Download/source/tv /share/Download/dst/tv
