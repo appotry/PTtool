e6cb8e15d45cff208770872276041e86abdef6b5
Ubuntu <zexin.dai@gmail.com>
2021-08-07 00:46:07 +0200

    add support for one file torrent

--- autolink.sh
+++ autolink.sh
@@ -10,21 +10,26 @@
 #qBittorrent %Z：Torrent 大小（字节）
 #qBittorrent %T：当前 tracker
 #qBittorrent %I：哈希值
-#在qBittorrent分别按如上顺序键入参数，ex:/path/to/autolink.sh "%N" "%F" "%L"
+#在qBittorrent分别按如上顺序键入参数，ex:/path/to/autolink.sh "%N" "%D" "%L"
 
 #your_path=/path/to/creat/link
 your_path=/mnt/nas/disk2/jellyfin
 
 #获取种子名称
 torrent_name="$1"
-#获取种子路径
+#获取保存路径
 torrent_path="$2"
-
 #获取种子分类
 torrent_category="$3"
 
-link_path_library="$your_path"/"$3"
+link_path_library="$your_path"/"$torrent_category"
 
-if [[ "$3" == *"movies"* || "$3" == *"series"* || "$3" == *"documents"* || "$3" == *"operas"* ]]; then
-  "$(dirname $(readlink -f $0))"/dirlink.sh "$2" "$link_path_library"
+if [[ "$torrent_category" == *"movies"* || "$torrent_category" == *"series"* || "$torrent_category" == *"documents"* || "$torrent_category" == *"operas"* ]]
+then
+  if [ -d "$torrent_path"/"$torrent_name" ]
+  then
+    "$(dirname $(readlink -f $0))"/dirlink.sh "$torrent_path" "$link_path_library"
+  else
+    "$(dirname $(readlink -f $0))"/dirlink.sh "$torrent_path"/.. "$link_path_library"
+  fi
 fi
